97ecbe0f2d49a27536a5df8eea2ffb05750d998f
Selene Waide <selenewaide@gmail.com>
2018-01-29 11:26:54 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# Movie Search Website
+
+This project renders multiple web pages. The first page allows the user to enter a movie title (a word or phrase), to find a collection of movies that match. The second page is rendered on clicking the 'Search' button. This displays a selection of movie thumbnails that matched the search, using OMDB api. The user can get additional details about a movie by clicking on the 'Details' button in each thumbnail. This diplays another page with the movie trailer (using Youtube api), and movie trivia from OMDB.
+
+## Table of Contents
+1. Getting Started
+2. Author
+3. Acknowledgments
+ 
+
+## 1. Getting Started
+
+These instructions will get you a copy of the project. 
+
+### Prerequisites
+
+* node.js
+* ejs
+* express
+* request
+* [Youtube API key](https://developers.google.com/youtube/v3/getting-started) 
+* [OMDB API key](http://www.omdbapi.com/apikey.aspx) 
+
+### Install & Run
+
+###### Step 1
+Clone the project from [Github Movie Search Project.](https://github.com/selenewaide/sw1-movie-search.git)
+```
+git clone https://github.com/selenewaide/sw1-movie-search.git
+```
+
+###### Step 2
+There are two variables at the top of the app.js file called omdb_api_key and youtube_api_key. The contents of these two variables needs to be amended with valid API keys obtained from OMDB and Youtube(v3).
+
+###### Step 3
+Run the app.js file to render the website.
+```
+node app.js
+```
+
+
+## 2. Author
+
+Selene Waide
+
+
+## 3. Acknowledgments
+
+Youtube and OMDB for use of their APIs.
---
+++ app.js
@@ -0,0 +1,85 @@
+var express = require("express");
+var app = express();
+var request = require("request");
+app.set("view engine", "ejs");
+
+// omdb url information
+var omdb_api_key = "&apikey=xxxxxxxxxx"; // Enter a valid api key here
+var omdb_base_url = "http://www.omdbapi.com/";
+
+// youtube url information
+var youtube_api_key = "&key=xxxxxxxxxxxxxxxxxxxx"; // Enter a valid api key here
+var url_youtube_search_1 = "https://www.googleapis.com/youtube/v3/search?part=id&q=";
+var url_youtube_search_2 = "+trailer&type=video";
+
+// error handling messages
+var error_msg_1 = "Unable to find a match for movie search. Check movie title/spelling and try again";
+var error_msg_2 = "Unable to get data from omdb.com for this movie.";
+
+// Added to recongnise public folder
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req, res) {
+    res.render("search");
+});
+
+app.get("/results", function(req, res) {
+    // get the data in 'search', from user input form
+    var query = (req.query.search).trim();
+    var url_omdb = omdb_base_url + "?s=" + query + omdb_api_key;
+
+    request(url_omdb, function(error, response, body) {
+        if(!error && response.statusCode == 200) {
+            // Convert body string that is returned to an object
+            var data = JSON.parse(body);
+
+            if(data["Response"] == "False") {
+                res.render("error", {error_msg: error_msg_1});
+            } else {
+                res.render("results", {data: data});
+            }
+        } else {
+            res.render("error", {error_msg: error_msg_1});
+        }
+    });
+});
+
+app.get("/details", function(req, res) {
+    // get the data in results
+    var imdb_id = req.query.imdb_id;
+    var url_omdb =  omdb_base_url + "?i=" + imdb_id + "&plot=full" + omdb_api_key;
+
+    request(url_omdb, function(error, response, body) {
+        if(!error && response.statusCode == 200) {
+            // Convert body string that is returned to an object
+            var movie_details = JSON.parse(body);
+
+            var search_title = movie_details["Title"];
+            var search_year = movie_details["Year"];
+            var search_phrase = search_title + " " + search_year;
+            var url_youtube = url_youtube_search_1 +
+                                search_phrase + url_youtube_search_2 +
+                                youtube_api_key;
+
+            request(url_youtube, function(error, response, body) {
+                if(!error && response.statusCode == 200) {
+                    // Convert body string that is returned to an object
+                    var movie_data_youtube = JSON.parse(body)
+                    res.render("details",
+                                {movie_data_youtube: movie_data_youtube,
+                                movie_details: movie_details});
+                } else {
+                    // this should be details with no youtube
+                    res.render("error");
+                }
+            });
+        } else {
+            res.render("error", {error_msg: error_msg_2});
+        }
+    });
+});
+
+
+app.listen(process.env.PORT || 5000);
+// app.listen(9000);
+
